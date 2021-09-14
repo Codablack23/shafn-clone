2492787b91808c42da2ad727f01aac479df9ce4f
sammyyo <erhini@yahoo.com>
2021-09-14 21:03:09 +0200

    wordPress changes

--- React-WordPress/repositories/WP/WPRepository.js
+++ React-WordPress/repositories/WP/WPRepository.js
@@ -9,15 +9,15 @@ import axios from 'axios';
 // const username = 'ck_7c8c5cfbeadd6d7780f88355fd393f2ee147a843';
 // const password = 'cs_d0cd71491fa24bf859e96d23b963e2c1b5ab9866';
 
-export const WPDomain = 'https://floppiess.com';
+// export const WPDomain = 'https://floppiess.com';
 
-const username = 'ck_07a17f9280703d8506668f42f0341da8f5b3b2c0';
-const password = 'cs_13fdec51c9df7aaa6a9c3ca8e7b9298377359c7b';
+// const username = 'ck_07a17f9280703d8506668f42f0341da8f5b3b2c0';
+// const password = 'cs_13fdec51c9df7aaa6a9c3ca8e7b9298377359c7b';
 
-//  export const WPDomain = 'http://shafn.floppiess.com';
+  export const WPDomain = 'http://shafn.com';
 
-//   const username = 'ck_47b61ccc6fefac46f01abf093fa7d26b067fb318';
-//   const password = 'cs_034b0cad7d15eef22bdb5d3ccb8ccfad47273bf9';
+  const username = 'ck_a1afc51ff351c0c57c490de46c158cb372e5dae7';
+  const password = 'cs_0f193ce5d8accbb6286ecc6675d1c78d1181c1f5';
 
 export const oathInfo = {
     consumer_key: username,
--- martfury-react-WordPress/components/elements/detail/modules/elements/ThumbnailImage.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { baseUrl } from '../../../../../repositories/Repository';
-
-const ThumbnailImage = ({ url }) => (
-    <img src={`${baseUrl}${url}`} alt="martfury-image" />
-);
-
-export default ThumbnailImage;
