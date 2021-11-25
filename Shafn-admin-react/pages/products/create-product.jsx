6b5d7cd93344a47d8cadadf6ece0f6ca55873d9f
VALENTINE ORGA <ValentineCodes>
2021-11-25 10:35:43 +0100

    Added multiple images to product upload

--- Shafn-admin-react/pages/products/create-product.jsx
+++ Shafn-admin-react/pages/products/create-product.jsx
@@ -3,6 +3,7 @@ import ContainerDefault from "~/components/layouts/ContainerDefault";
 import HeaderDashboard from "~/components/shared/headers/HeaderDashboard";
 import { connect, useDispatch } from "react-redux";
 import axios from "axios";
+import { notification } from "antd";
 import { toggleDrawerMenu } from "~/store/app/action";
 
 const CreateProductPage = () => {
@@ -15,65 +16,68 @@ const CreateProductPage = () => {
   const [category, setCategory] = useState("");
   const [qty, setQty] = useState("");
   const [sku, setSku] = useState("");
-  const [image, setImage] = useState("");
-  const [imagePreview, setImagePreview] = useState("");
+  const [imageFiles, setImageFiles] = useState([]);
+  const [selectedImages, setSelectedImages] = useState([]);
 
-  const [imageUploadStatus, setImageUploadStatus] = useState("NOT_UPLOADING");
+  // const [imageUploadStatus, setImageUploadStatus] = useState("NOT_UPLOADING");
   const [isUploading, setIsUploading] = useState(false);
 
   const imageHandler = (e) => {
     //Display Image
-    const reader = new FileReader();
+    e.persist();
+    if (e.target.files) {
+      setImageFiles((current) => [...current, ...e.target.files]);
 
-    reader.onload = () => {
-      if (reader.readyState === 2) {
-        setImagePreview(reader.result);
-      }
-    };
+      const imgArr = Array.from(e.target.files).map((img) => ({
+        name: img.name,
+        url: URL.createObjectURL(img),
+      }));
 
-    reader.readAsDataURL(e.target.files[0]);
+      setSelectedImages((current) => current.concat(imgArr));
 
-    // Upload Images
-
-    const formData = new FormData();
-
-    formData.append("file", e.target.files[0]);
-
-    let auth_token = localStorage.getItem("auth_token");
-
-    const headers = {
-      headers: {
-        Authorization: `Bearer ${auth_token}`,
-      },
-    };
-
-    setImageUploadStatus("UPLOADING");
-
-    axios
-      .post("https://shafn.com/wp-json/wp/v2/media", formData, headers)
-      .then((res) => {
-        setImageUploadStatus("UPLOADED");
-        setImage([
-          {
-            src: res.data.source_url,
-          },
-        ]);
-      })
-      .catch((err) => {
-        setImageUploadStatus("FAILED");
-      });
+      Array.from(e.target.files).map((img) => URL.revokeObjectURL(img));
+    }
   };
 
-  const handleOnSubmit = (e) => {
-    e.preventDefault();
-    let auth_token = localStorage.getItem("auth_token");
+  const uploadImages = (config) => {
+    // setImageUploadStatus("UPLOADING");
 
-    const config = {
-      headers: {
-        Authorization: `Bearer ${auth_token}`,
-      },
-    };
+    let images = [];
+    imageFiles.forEach((img, index) => {
+      let formData = new FormData();
 
+      formData.append("file", img);
+
+      axios
+        .post("https://shafn.com/wp-json/wp/v2/media", formData, config)
+        .then((res) => {
+          // setImageUploadStatus("UPLOADED");
+
+          images.push({ src: res.data.source_url, position: index });
+        })
+        .catch((err) => {
+          // setImageUploadStatus("FAILED");
+          return;
+        })
+        .finally(() => {
+          //  Check if last image has been reached
+          if (index === imageFiles.length - 1) {
+            // Check if every image uploaded
+            if (images.length === imageFiles.length) {
+              uploadProduct(config, images);
+            } else {
+              setIsUploading(false);
+              notification["error"]({
+                message:
+                  "Some images did not upload!. Check your data connection and try again.",
+              });
+            }
+          }
+        });
+    });
+  };
+
+  const uploadProduct = (config, images) => {
     let slug = `${name
       .replace(/[^a-zA-Z0-9-_]/g, " ")
       .replace(/  +/g, " ")
@@ -90,23 +94,72 @@ const CreateProductPage = () => {
       categories: category,
       stock_quantity: Number(qty.trim()),
       sku,
-      images: image,
+      images,
     };
-
-    // Upload Images
-
-    setIsUploading(true);
 
     axios
       .post("https://shafn.com/wp-json/dokan/v1/products/", product, config)
       .then((res) => {
+        notification["success"]({
+          message: "Product Uploaded Successfully",
+        });
         setIsUploading(false);
-        alert("Product Uploaded Successfully");
+        setName("");
+        setPrice("");
+        setDiscountedPrice("");
+        setDescription("");
+        setCategory("");
+        setQty("");
+        setSku("");
+        setImageFiles([]);
+        setSelectedImages([]);
       })
       .catch((err) => {
+        notification["error"]({
+          message: "Product Upload Failed",
+          description: "Check your data connection and try again.",
+        });
         setIsUploading(false);
-        console.log("Error::", err.response);
       });
+  };
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+
+    setIsUploading(true);
+
+    let auth_token = localStorage.getItem("auth_token");
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${auth_token}`,
+      },
+    };
+
+    uploadImages(config); // product is uploaded in here
+  };
+
+  const removeImage = (name) => {
+    setSelectedImages((current) => current.filter((img) => img.name !== name));
+    setImageFiles((current) => current.filter((img) => img.name !== name));
+  };
+
+  const renderImages = () => {
+    return selectedImages.map((img, index) => (
+      <div key={img.url} style={styles.imageContainer}>
+        <img
+          src={img.url}
+          style={{
+            width: 200,
+            maxHeight: 300,
+          }}
+        />
+
+        <div style={styles.imageDel} onClick={removeImage.bind(this, img.name)}>
+          x
+        </div>
+      </div>
+    ));
   };
 
   useEffect(() => {
@@ -285,6 +338,7 @@ const CreateProductPage = () => {
                           accept="image/*"
                           onChange={imageHandler}
                           required
+                          multiple
                           hidden
                         />
                         <label
@@ -294,7 +348,7 @@ const CreateProductPage = () => {
                         >
                           Choose
                         </label>
-                        {imageUploadStatus === "UPLOADING" ? (
+                        {/* {imageUploadStatus === "UPLOADING" ? (
                           <img
                             src={require("../../public/img/Interwind-loader.svg")}
                             alt="Loading..."
@@ -323,7 +377,7 @@ const CreateProductPage = () => {
                               height: 20,
                             }}
                           />
-                        ) : null}
+                        ) : null} */}
                       </div>
                     </div>
                     {/* <div className="form-group form-group--nest">
@@ -343,12 +397,9 @@ const CreateProductPage = () => {
                       />
                     </div> */}
 
-                    {imagePreview && (
-                      <img
-                        src={imagePreview}
-                        style={{ width: 200, maxHeight: 300 }}
-                      />
-                    )}
+                    <div style={{ display: "flex", flexWrap: "wrap" }}>
+                      {renderImages()}
+                    </div>
                   </div>
                 </figure>
                 <figure className="ps-block--form-box">
@@ -428,3 +479,32 @@ const CreateProductPage = () => {
   );
 };
 export default connect((state) => state.app)(CreateProductPage);
+
+let styles = {
+  imageContainer: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    width: 200,
+    maxHeight: 300,
+    backgroundColor: "black",
+    marginLeft: 20,
+    marginBottom: 10,
+    position: "relative",
+  },
+  imageDel: {
+    position: "absolute",
+    backgroundColor: "black",
+    color: "red",
+    fontSize: 15,
+    top: 5,
+    right: 5,
+    width: 30,
+    height: 30,
+    borderRadius: 50,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    cursor: "pointer",
+  },
+};
