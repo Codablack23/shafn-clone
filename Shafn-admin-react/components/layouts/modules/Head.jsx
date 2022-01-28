96c7754c938c33e3e66789ae90ab21c0a588d534
Codablack23 <77158925+Codablack23@users.noreply.github.com>
2022-01-28 11:59:53 +0100

    fixed merging conflict in head.jsx in admin folder

--- Shafn-admin-react/components/layouts/modules/Head.jsx
+++ Shafn-admin-react/components/layouts/modules/Head.jsx
@@ -17,11 +17,7 @@ const StyleSheets = () => (
             name="description"
             content="Shafn"
         />
-<<<<<<< HEAD
         <title>Martfury | React eCommerce Admin Template</title>
-=======
-        <title>Shafn</title>
->>>>>>> d131f8d71a60a882ca0ce24e0af61db69361c3f3
         <link
             href="https://fonts.googleapis.com/css?family=Work+Sans:300,400,500,600,700&amp;amp;subset=latin-ext"
             rel="stylesheet"
@@ -36,10 +32,6 @@ const StyleSheets = () => (
             href="/fonts/font-awesome/css/font-awesome.min.css"
         />
         <link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css" />
-<<<<<<< HEAD
-=======
-
->>>>>>> d131f8d71a60a882ca0ce24e0af61db69361c3f3
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossOrigin="anonymous"></script>
     </Head>
 );
