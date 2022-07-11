980f8031fa12d0264935643526ae4d08013a0b06
VALENTINE ORGA <ValentineCodes>
2022-07-11 14:34:04 +0100

    checking social media button issue...

--- React-WordPress/components/elements/media/SocialShareButtons.jsx
+++ React-WordPress/components/elements/media/SocialShareButtons.jsx
@@ -8,28 +8,26 @@ import {
 
 const SocialShareButtons = ({ url }) => (
     <>
-        <FacebookShareButton url={url}>
-            {/* <a className="social-link">
-                <i className="bi bi-facebook"></i>
-            </a> */}
-            <span>Facebook</span>
-        </FacebookShareButton>
-        <TwitterShareButton url={url}>
-            {/* <a className="social-link">
-                <i className="bi bi-twitter"></i>
-            </a> */}
-            <span>Twitter</span>
-        </TwitterShareButton>
-        <LinkedinShareButton url={url}>
-            <a className="social-link">
-                <i className="bi bi-linkedin" style={{ color: '#0073B1' }}></i>
-            </a>
-        </LinkedinShareButton>
-        <PinterestShareButton url={url}>
-            <a className="social-link">
-                <i className="bi bi-pinterest"></i>
-            </a>
-        </PinterestShareButton>
+        {/* <FacebookShareButton url={url}> */}
+        <a className="social-link">
+            <i className="bi bi-facebook"></i>
+        </a>
+        {/* </FacebookShareButton>
+        <TwitterShareButton url={url}> */}
+        <a className="social-link">
+            <i className="bi bi-twitter"></i>
+        </a>
+        {/* </TwitterShareButton>
+        <LinkedinShareButton url={url}> */}
+        <a className="social-link">
+            <i className="bi bi-linkedin" style={{ color: '#0073B1' }}></i>
+        </a>
+        {/* </LinkedinShareButton>
+        <PinterestShareButton url={url}> */}
+        <a className="social-link">
+            <i className="bi bi-pinterest"></i>
+        </a>
+        {/* </PinterestShareButton> */}
     </>
 );
 
