12f62403135c39cca6ffbbd798280b10ac0d03ef
Codablack <goodluckedih@yahoo.com>
2022-07-20 09:57:35 +0100

    added privacy policy page

--- React-WordPress/components/elements/modalCookie.js
+++ React-WordPress/components/elements/modalCookie.js
@@ -1,9 +1,10 @@
+import Link from "next/link";
 import { useCookies } from "react-cookie";
 
 const HideCookiePolicy = ()=>{
      const cookiePopUp = document.getElementById('cookie-popup')
      const cookieWidget = document.getElementById('cookieWidget')
-     console.log(cookiePopUp)
+     console.log(cookiePopUp.style.opacity)
      if(cookiePopUp.style.opacity === "1"){
         cookiePopUp.style.opacity = 0
         cookiePopUp.style.zIndex= -1
@@ -21,11 +22,12 @@ const HideCookiePolicy = ()=>{
 export default function ModalCookie(){
     const [cookies, setCookies] = useCookies()
     function CreateCookie(status) {
+      HideCookiePolicy()
         setCookies("ShafN-cookie-policy",status, {
           path: "/",
           secure:true
         });
-        HideCookiePolicy()
+    
       }
     console.log(cookies)
    
@@ -34,9 +36,11 @@ export default function ModalCookie(){
              <div className="ps__cookie-popup" id="cookie-popup">
                 <span className="close" onClick={HideCookiePolicy}><i className="bi bi-x-lg"></i></span>
                 <h4 className="title">This website uses cookies</h4>
-                <p>We use cookies to operate this website improve usability and personalize your experience, feel free to make a choice to accept or reject.</p>
+                <p>We use cookies to operate this website improve usability and keep track of your products to  personalize your experience,</p>
                 <button className="accept" onClick={()=>CreateCookie("accepted")}>Accept all cookies</button>
-                <button className="reject" onClick={()=>CreateCookie("rejected")}>Reject all cookies</button>
+                <Link href={"/privacy-policy"}>
+                <button className="reject">View Our Cookie Policy</button>
+                </Link>
             </div>
             <div className="ps__cookie-popup-widget" id="cookieWidget">
                 <button onClick={HideCookiePolicy}>
---
+++ React-WordPress/pages/privacy-policy.jsx
@@ -0,0 +1,43 @@
+import React, { useEffect,useState } from 'react';
+import WPLayoutHomeDefault from '~/wp-components/layouts/WPLayoutHomeDefault';
+import ModalCookie from '~/components/elements/modalCookie';
+import { motion } from 'framer-motion';
+
+export default function PrivacyPolicy(){
+    const [opacity,setOpacity] = useState(0)
+    useEffect(()=>{
+
+          setTimeout(()=>{
+            setOpacity(1)
+          },5000)
+    },[])
+    return(
+        <WPLayoutHomeDefault title="Multipurpose Marketplace React Ecommerce Template">
+            <div className="ps__privacy-policy-page container">
+                <br />
+                <h2 className='text-center'>Privacy Policy</h2><br />
+                <div className="ps__policy-section">
+                  <p className='title'>Why do we collect cookies?</p>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, necessitatibus eligendi officiis dolore numquam doloremque nam earum nostrum vitae assumenda rerum quae, ducimus possimus dolorum corporis unde nemo expedita sequi. Odio pariatur eligendi enim quaerat distinctio accusantium deleniti vero nam eum perspiciatis, dolorum repellat voluptatum reprehenderit quod quas facere eaque aliquid, doloribus veniam quasi ab omnis eveniet. Delectus obcaecati totam facilis accusantium id magni possimus itaque quaerat fugiat quasi. Facilis magni a natus? Inventore sint quos asperiores, ad quibusdam dolorem labore illum iure minima, delectus id aliquid? Dicta, voluptas ullam labore facere quis, iure doloremque quidem beatae animi neque repellendus.</p>
+                </div>
+                <div className="ps__policy-section">
+                  <p className='title'>What we use cookies for</p>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, necessitatibus eligendi officiis dolore numquam doloremque nam earum nostrum vitae assumenda rerum quae, ducimus possimus dolorum corporis unde nemo expedita sequi. Odio pariatur eligendi enim quaerat distinctio accusantium deleniti vero nam eum perspiciatis, dolorum repellat voluptatum reprehenderit quod quas facere eaque aliquid, doloribus veniam quasi ab omnis eveniet. Delectus obcaecati totam facilis accusantium id magni possimus itaque quaerat fugiat quasi. Facilis magni a natus? Inventore sint quos asperiores, ad quibusdam dolorem labore illum iure minima, delectus id aliquid? Dicta, voluptas ullam labore facere quis, iure doloremque quidem beatae animi neque repellendus.</p>
+                </div>
+                <div className="ps__policy-section">
+                  <p className='title'>What information we collect</p>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, necessitatibus eligendi officiis dolore numquam doloremque nam earum nostrum vitae assumenda rerum quae, ducimus possimus dolorum corporis unde nemo expedita sequi. Odio pariatur eligendi enim quaerat distinctio accusantium deleniti vero nam eum perspiciatis, dolorum repellat voluptatum reprehenderit quod quas facere eaque aliquid, doloribus veniam quasi ab omnis eveniet. Delectus obcaecati totam facilis accusantium id magni possimus itaque quaerat fugiat quasi. Facilis magni a natus? Inventore sint quos asperiores, ad quibusdam dolorem labore illum iure minima, delectus id aliquid? Dicta, voluptas ullam labore facere quis, iure doloremque quidem beatae animi neque repellendus.</p>
+                </div>
+                  <div className="ps__policy-section">
+                  <p className='title'>Are your Information Safe?</p>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, necessitatibus eligendi officiis dolore numquam doloremque nam earum nostrum vitae assumenda rerum quae, ducimus possimus dolorum corporis unde nemo expedita sequi. Odio pariatur eligendi enim quaerat distinctio accusantium deleniti vero nam eum perspiciatis, dolorum repellat voluptatum reprehenderit quod quas facere eaque aliquid, doloribus veniam quasi ab omnis eveniet. Delectus obcaecati totam facilis accusantium id magni possimus itaque quaerat fugiat quasi. Facilis magni a natus? Inventore sint quos asperiores, ad quibusdam dolorem labore illum iure minima, delectus id aliquid? Dicta, voluptas ullam labore facere quis, iure doloremque quidem beatae animi neque repellendus.</p>
+                </div>
+            </div>
+        <motion.div animate={{opacity:opacity}}>
+          <div>
+              <ModalCookie/>
+          </div>
+        </motion.div>      
+    </WPLayoutHomeDefault>
+    )
+}
