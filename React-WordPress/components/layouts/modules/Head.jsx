808601e3729dcab5e224cbd92d924f24ca250e5a
Codablack <goodluckedih@yahoo.com>
2022-08-18 15:41:33 +0100

    fixed warnings

--- React-WordPress/components/layouts/modules/Head.jsx
+++ React-WordPress/components/layouts/modules/Head.jsx
@@ -29,32 +29,32 @@ const StyleSheets = () => (
         {/*/>*/}
         {/*<link rel="apple-touch-icon" href="/apple-icon-60x60-dunplab-manifest-15113.png"></link>*/}
         {/*<meta name="theme-color" content="#fcb800" />*/}
-
+{/* 
         <link
             rel="stylesheet"
             type="text/css"
             href="https://www.w3schools.com/w3css/4/w3.css"
-        />
+        /> */}
 
-        <link
-            href="https://fonts.googleapis.com/css?family=Work+Sans:300,400,500,600,700&amp;amp;subset=latin-ext"
+        {/* <link
+            href=""
             rel="stylesheet"
-        />
-        <link
+        /> */}
+        {/* <link
             rel="stylesheet"
             href="/static/fonts/Linearicons/Font/demo-files/demo.css"
-        />
+        /> */}
 
-        <link
+        {/* <link
             rel="stylesheet"
             href="/static/fonts/font-awesome/css/font-awesome.min.css"
-        />
+        /> */}
 
-        <link
+        {/* <link
             rel="stylesheet"
             type="text/css"
             href="/static/css/bootstrap.min.css"
-        />
+        /> */}
         <link rel="stylesheet" type="text/css" href="/static/css/custom.css" />
     </Head>
 );
