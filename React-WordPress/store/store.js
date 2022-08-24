b00bad8106d24632c17880f0d9469f139acba10d
VALENTINE ORGA <ValentineCodes>
2022-08-24 06:37:04 +0100

    update: redux

--- React-WordPress/store/store.js
+++ React-WordPress/store/store.js
@@ -1,10 +1,18 @@
-import { applyMiddleware, createStore } from "redux";
+import { applyMiddleware, legacy_createStore as createStore } from "redux";
 import createSagaMiddleware from "redux-saga";
 import { persistReducer } from "redux-persist";
 import createWebStorage from "redux-persist/lib/storage/createWebStorage";
 
 import rootReducer from "./rootReducer";
 import rootSaga from "./rootSaga";
+
+const bindMiddleware = (middleware) => {
+    if (process.env.NODE_ENV !== "production") {
+        const { composeWithDevTools } = require("redux-devtools-extension");
+        return composeWithDevTools(applyMiddleware(...middleware));
+    }
+    return applyMiddleware(...middleware);
+};
 
 const createNoopStorage = () => {
     return {
@@ -24,14 +32,6 @@ const storage =
     typeof window !== "undefined"
         ? createWebStorage("local")
         : createNoopStorage();
-
-const bindMiddleware = (middleware) => {
-    if (process.env.NODE_ENV !== "production") {
-        const { composeWithDevTools } = require("redux-devtools-extension");
-        return composeWithDevTools(applyMiddleware(...middleware));
-    }
-    return applyMiddleware(...middleware);
-};
 
 const persistConfig = {
     key: "martfury",
