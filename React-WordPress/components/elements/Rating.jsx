f1f525144ad5602c9c3d7840bc58500721314758
VALENTINE ORGA <ValentineCodes>
2022-08-29 13:22:08 +0100

    update: displayed ratings on product card

--- React-WordPress/components/elements/Rating.jsx
+++ React-WordPress/components/elements/Rating.jsx
@@ -1,19 +1,23 @@
-import React from 'react';
-function generateFilledStars(rating){
-   const filledStars = new Array(rating).fill("").map((i,r)=><i key={`${r}-filled`} className="fa fa-star"></i>)
-   return filledStars
+import React from "react";
+function generateFilledStars(rating) {
+    const filledStars = new Array(rating)
+        .fill("")
+        .map((i, r) => <i key={`${r}-filled`} className="fa fa-star"></i>);
+    return filledStars;
 }
-function generateEmptyStars(rating){
-    const emptyStars = new Array(rating).fill("").map((i,r)=><i key={`${r}-empty`} className="fa fa-star-o"></i>)
-    return emptyStars
+function generateEmptyStars(rating) {
+    const emptyStars = new Array(rating)
+        .fill("")
+        .map((i, r) => <i key={`${r}-empty`} className="fa fa-star-o"></i>);
+    return emptyStars;
 }
-function isNaN(num){
-   return typeof(num)=== "number"?num:0
+function isNaN(num) {
+    return typeof num === "number" ? num : 0;
 }
-const Rating = ({rating}) => (
+const Rating = ({ rating }) => (
     <span className="ps-rating">
-       {generateFilledStars(rating?rating:0)}
-       {generateEmptyStars(5-(isNaN(rating)))}
+        {generateFilledStars(rating ? rating : 0)}
+        {generateEmptyStars(5 - isNaN(rating))}
     </span>
 );
 
