9b142fad5497fd22740f69b1ae1030b1f686bc19
VALENTINE ORGA <ValentineCodes>
2022-09-02 15:12:03 +0100

    update: added consumer key and secret as env variables

--- Shafn-admin-react/repositories/Repository.js
+++ Shafn-admin-react/repositories/Repository.js
@@ -1,20 +1,20 @@
-import axios from "axios";
-export const isGrapql = true;
-const baseDomain = "http://localhost:1337";
+import axios from "axios"
+export const isGrapql = true
+const baseDomain = "http://localhost:1337"
 /*const baseDomain = 'http://45.76.97.89:1337';*/
-const authorization_prefix = "Bearer ";
+const authorization_prefix = "Bearer "
 export const customHeaders = {
   Accept: "application/json",
   /* Authorization: authorization_prefix + token || undefined*/
-};
+}
 
-export const baseUrl = `${baseDomain}`;
-export const WPDomain = "https://shafn.com";
+export const baseUrl = `${baseDomain}`
+export const WPDomain = "https://shafn.com"
 
 export default axios.create({
   baseUrl,
   headers: customHeaders,
-});
+})
 
 export async function fetchData(query) {
   const response = await axios({
@@ -24,23 +24,20 @@ export async function fetchData(query) {
     data: {
       query: query,
     },
-  });
+  })
 
-  return response;
+  return response
 }
 
-const username = "ck_a1afc51ff351c0c57c490de46c158cb372e5dae7";
-const password = "cs_0f193ce5d8accbb6286ecc6675d1c78d1181c1f5";
-
 export const oathInfo = {
-  consumer_key: username,
-  consumer_secret: password,
-};
+  consumer_key: process.env.consumer_key,
+  consumer_secret: process.env.consumer_secret,
+}
 
 export const serializeQuery = (query) => {
   return Object.keys(query)
     .map(
       (key) => `${encodeURIComponent(key)}=${encodeURIComponent(query[key])}`
     )
-    .join("&");
-};
+    .join("&")
+}
