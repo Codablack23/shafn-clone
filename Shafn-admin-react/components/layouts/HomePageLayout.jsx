690efebccf2dc7588778b92751f316eb99363ce2
VALENTINE ORGA <ValentineCodes>
2022-09-04 00:54:15 +0100

    update: added link to CTA button

--- Shafn-admin-react/components/layouts/HomePageLayout.jsx
+++ Shafn-admin-react/components/layouts/HomePageLayout.jsx
@@ -1,17 +1,16 @@
-import Head from "next/head";
-import Header from "../home/Header";
-import ModalCookie from "../home/modalCookie";
+import Head from "next/head"
+import Header from "../home/Header"
+import ModalCookie from "../home/modalCookie"
 
-export default function HomepageLayout({children,title,page}){
-    return (
-        <div className="ps__vendor-homepage">
-        <Head>
-            <title>ShafN | {title?title:'Home'}</title>
-        </Head>
-        <Header/>
-        {children}
-        {page != "accounts" &&  <ModalCookie/>}
-        
-        </div>
-    )
+export default function HomepageLayout({ children, title, page }) {
+  return (
+    <div className="ps__vendor-homepage">
+      <Head>
+        <title>ShafN | {title ? title : "Home"}</title>
+      </Head>
+      <Header />
+      {children}
+      {page != "accounts" && <ModalCookie />}
+    </div>
+  )
 }
