131ce347d9e57af597170c27c29d80a4f364469b
VALENTINE ORGA <ValentineCodes>
2022-09-05 11:56:51 +0100

    update

--- React-WordPress/components/partials/account/OrderListing.jsx
+++ React-WordPress/components/partials/account/OrderListing.jsx
@@ -1,93 +1,93 @@
 import React, { useState } from "react";
 import { Select, Table } from "antd";
 
-export default function OrdersListing(){
-    const {Option} = Select
-    const [userOrders,setUsersOrders] = useState([
+export default function OrdersListing() {
+    const { Option } = Select;
+    const [userOrders, setUsersOrders] = useState([
         {
-          key: '1',
-          id:"1",
-          customer: 'John Brown',
-          total: 32,
-          status: "pending",
-          date: new Date().toDateString(),
+            key: "1",
+            id: "1",
+            customer: "John Brown",
+            total: 32,
+            status: "pending",
+            date: new Date().toDateString(),
         },
-      ])
-    const status =[
-     "Pending",
-     "Processing",
-     "On-hold",
-     "Completed",
-     "Cancelled",
-     "Refunded",
-     "Failed",
-    ]
+    ]);
+    const status = [
+        "Pending",
+        "Processing",
+        "On-hold",
+        "Completed",
+        "Cancelled",
+        "Refunded",
+        "Failed",
+    ];
     const columns = [
         {
-          title: 'ID',
-          dataIndex: 'id',
-          key: 'id',
-          render: (text) => <a>{text}</a>,
+            title: "ID",
+            dataIndex: "id",
+            key: "id",
+            render: (text) => <a>{text}</a>,
         },
         {
-          title: 'Total',
-          dataIndex: 'total',
-          key: 'total',
+            title: "Total",
+            dataIndex: "total",
+            key: "total",
         },
         {
-          title: 'Status',
-          dataIndex: 'status',
-          key: 'status',
+            title: "Status",
+            dataIndex: "status",
+            key: "status",
         },
         {
-          title: 'Customer',
-          dataIndex: 'customer',
-          key: 'customer',
+            title: "Customer",
+            dataIndex: "customer",
+            key: "customer",
         },
         {
-        title: 'Date',
-        dataIndex: 'date',
-        key: 'date',
+            title: "Date",
+            dataIndex: "date",
+            key: "date",
         },
-    ]
-    return(
+    ];
+    return (
         <div className="orders-listing">
-         <header>
-            <p className="ps__orders-heading">Orders</p>
-            <p className="sub-heading">your Order's Listing</p>
-            <div className="d-flex new_order justify-content-end mt-3">
-                <button>
-                    <span>
-                    <i className="bi bi-plus-lg"></i>
-                    New Order
-                    </span>
+            <header>
+                <p className="ps__orders-heading">Orders</p>
+                <p className="sub-heading">Your Order's Listing</p>
+            </header>
+            <section className="filter-container mt-5 d-lg-flex align-items-center">
+                <input type="text" placeholder="Search" />
+                <div className="select-component">
+                    <Select
+                        className="select-element"
+                        style={{ width: "100%" }}>
+                        {status.map((s, i) => (
+                            <Option key={`${i}-orders`} value={s}>
+                                {s}
+                            </Option>
+                        ))}
+                    </Select>
+                </div>
+                <button type="button" className="filter-button">
+                    Filter
                 </button>
-            </div>
-         </header>   
-         <section className="filter-container mt-5 d-lg-flex align-items-center">
-            <input type="text" placeholder="Search" />
-            <div className="select-component">
-            <Select className="select-element" style={{width:"100%"}}>
-               {status.map((s,i)=>(
-                <Option key={`${i}-orders`} value={s}>{s}</Option>
-               ))}
-            </Select>
-            </div>
-            <button type="button" className="filter-button">Filter</button>
-         </section><br />
-         <section>
-         <Table 
-           columns={columns} 
-           dataSource={userOrders} scroll={{x:20}}
-           onRow={(row)=>{
-            return{
-                onClick:()=>{
-                    console.log(row)
-                }
-            }
-           }}
-           />
-         </section>
+            </section>
+            <br />
+            <section>
+                <Table
+                    columns={columns}
+                    dataSource={userOrders}
+                    scroll={{ x: 20 }}
+                    onRow={(row) => {
+                        return {
+                            onClick: () => {
+                                console.log(row);
+                            },
+                        };
+                    }}
+                />
+            </section>
         </div>
-    )
+    );
 }
