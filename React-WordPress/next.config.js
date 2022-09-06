be2f15fd977e4ff210108b37b436ef9e1545b8ac
ValentineCodes <61652529+ValentineCodes@users.noreply.github.com>
2022-09-06 15:43:57 +0100

    added config file

---
+++ React-WordPress/next.config.js
@@ -0,0 +1,23 @@
+const withPlugins = require("next-compose-plugins");
+const withImages = require("next-images");
+
+const nextSettings = {
+    env: {
+        title: "ShafN",
+        titleDescription: "We are here to serve you",
+    },
+    swcMinify: false,
+};
+
+module.exports = {
+    async rewrites() {
+        return [
+            {
+                source: "/api/:path*",
+                destination: "https://shafn.com/:path*",
+            },
+        ];
+    },
+};
+
+module.exports = withPlugins([withImages(), nextSettings]);
