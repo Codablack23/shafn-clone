b629abd60deef5759211fe9a0ef875446f70046d
VALENTINE ORGA <ValentineCodes>
2022-09-07 22:16:53 +0100

    prevent access to customer profile if logged out

--- React-WordPress/pages/account/addresses.jsx
+++ React-WordPress/pages/account/addresses.jsx
@@ -1,18 +1,28 @@
-import React from 'react';
-// import Newsletters from '~/components/partials/commons/Newletters';
-import Addresses from '~/components/partials/account/Addresses';
-import WPLayout from '~/wp-components/layouts/WPLayout';
-import { scrollPageToTop } from '~/utilities/common-helpers';
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
+import Router from "next/router";
+import Addresses from "~/components/partials/account/Addresses";
+import WPLayout from "~/wp-components/layouts/WPLayout";
+import { scrollPageToTop } from "~/utilities/common-helpers";
 
-const MyAccountPage = () => (
-    <div ref={scrollPageToTop}>
-        <WPLayout title="Address">
-            <div className="ps-page--my-account">
-                <Addresses />
-            </div>
-            {/* <Newsletters layout="container" /> */}
-        </WPLayout>
-    </div>
-);
+const MyAccountPage = () => {
+    const auth = useSelector((state) => state.auth);
+
+    useEffect(() => {
+        if (!auth.isLoggedIn) {
+            Router.push("/account/login");
+        }
+    }, []);
+
+    return (
+        <div ref={scrollPageToTop}>
+            <WPLayout title="Address">
+                <div className="ps-page--my-account">
+                    <Addresses />
+                </div>
+            </WPLayout>
+        </div>
+    );
+};
 
 export default MyAccountPage;
--- React-WordPress/pages/account/edit-billing-address.jsx
+++ React-WordPress/pages/account/edit-billing-address.jsx
@@ -1,18 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
+import Router from "next/router";
 
 import EditBillingAddress from "~/components/partials/account/EditBillingAddress";
 import { scrollPageToTop } from "~/utilities/common-helpers";
 
 import WPLayout from "~/wp-components/layouts/WPLayout";
 
-const MyAccountPage = () => (
-    <div ref={scrollPageToTop}>
-        <WPLayout>
-            <div className="ps-page--my-account">
-                <EditBillingAddress />
-            </div>
-        </WPLayout>
-    </div>
-);
+const MyAccountPage = () => {
+    const auth = useSelector((state) => state.auth);
+
+    useEffect(() => {
+        if (!auth.isLoggedIn) {
+            Router.push("/account/login");
+        }
+    }, []);
+
+    return (
+        <div ref={scrollPageToTop}>
+            <WPLayout>
+                <div className="ps-page--my-account">
+                    <EditBillingAddress />
+                </div>
+            </WPLayout>
+        </div>
+    );
+};
 
 export default MyAccountPage;
