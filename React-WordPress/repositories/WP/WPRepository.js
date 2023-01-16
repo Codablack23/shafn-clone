4049997cb90bfc4a9cb64047e43c0fd2306784a9
ValentineCodes <Valentineorga@gmail.com>
2023-01-16 10:10:37 -0800

    set consumer key and secret to render products from woocommerce

--- React-WordPress/repositories/WP/WPRepository.js
+++ React-WordPress/repositories/WP/WPRepository.js
@@ -12,8 +12,8 @@ export const DashboardDomain =
         : VendorDomain;
 
 export const oathInfo = {
-    consumer_key: process.env.CONSUMER_KEY,
-    consumer_secret: process.env.CONSUMER_SECRET,
+    consumer_key: "ck_8780f4e0e8de23f507553d4ba0c781ebd1dcd635",
+    consumer_secret: "cs_ec782c1132885e67a5f955b0cf811d88e08186ad",
 };
 
 export const customHeaders = {
