f34f60dbf30d7839606ce506de3aea9db6c6c219
ValentineCodes <Valentineorga@gmail.com>
2023-01-18 12:26:20 -0800

    updated vendor page domain in development from localhost:5000 to localhost:5500

--- React-WordPress/repositories/WP/WPRepository.js
+++ React-WordPress/repositories/WP/WPRepository.js
@@ -5,7 +5,7 @@
 import axios from "axios";
 
 export const WPDomain = "https://api.shafn.com";
-export const VendorDomain = "http://localhost:5000";
+export const VendorDomain = "http://localhost:5500";
 export const DashboardDomain =
     process.env.NODE_ENV === "production"
         ? "https://www.seller.shafn.com"
