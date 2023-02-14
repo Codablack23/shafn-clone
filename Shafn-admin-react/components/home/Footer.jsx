b77dc67197d1c29db637fdd8989454367b49f48c
ValentineCodes <Valentineorga@gmail.com>
2023-02-14 08:56:30 -0800

    centered dashboard home components

--- Shafn-admin-react/components/home/Footer.jsx
+++ Shafn-admin-react/components/home/Footer.jsx
@@ -1,14 +1,14 @@
 import FooterCopyright from "./modules/FooterCopyright";
 import FooterWidgets from "./modules/FooterWidgets";
 
-export default function Footer(){
-    return(
-        <footer className="ps-footer">
-        <div className="container">
-            <FooterWidgets />
-            {/* <FooterLinks /> */}
-            <FooterCopyright />
-        </div>
+export default function Footer() {
+  return (
+    <footer className="ps-footer">
+      <div className="ps-footer__content_wrapper container">
+        <FooterWidgets />
+        {/* <FooterLinks /> */}
+        <FooterCopyright />
+      </div>
     </footer>
-    )
+  );
 }
