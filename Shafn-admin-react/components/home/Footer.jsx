7a47e82b8a92751d72917c78d42465954dbdb0c7
Goodluck Edih <goodluckedih@yahoo.com>
2024-05-06 13:14:57 +0100

    adjusted footer in vendors page

--- Shafn-admin-react/components/home/Footer.jsx
+++ Shafn-admin-react/components/home/Footer.jsx
@@ -4,7 +4,7 @@ import FooterWidgets from "./modules/FooterWidgets";
 export default function Footer() {
   return (
     <footer className="ps-footer">
-      <div className="ps-footer__content_wrapper container">
+      <div className="ps-footer__content_wrapper">
         <FooterWidgets />
         {/* <FooterLinks /> */}
         <FooterCopyright />
