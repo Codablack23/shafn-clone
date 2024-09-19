f5a9fa68e041e0d67bf445265a106d43ae9c2df8
sammyyo <erhini@eyahoo.com>
2024-09-19 17:17:57 +0200

    little fix

--- Shafn-admin-react/components/home/modules/FooterCopyright.jsx
+++ Shafn-admin-react/components/home/modules/FooterCopyright.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const FooterCopyright = () => (
     <div className="ps-footer__copyright" style={{display:"flex", justifyContent: "center"}}>
-        <p className="text-center">&copy; Shafn {new Date().getFullYear()} All Rights Reserved</p>
+        <p className="text-center">&copy; ShafN {new Date().getFullYear()} All Rights Reserved</p>
     </div>
 );
 
