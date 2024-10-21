8040a017f4a3cbadc884e51ca5a1a3feddaacf4b
Aeroox <kayroog1@gmail.com>
2024-10-21 10:41:14 +0100

    footer-fix

--- Shafn-admin-react/components/home/modules/FooterWidgets.jsx
+++ Shafn-admin-react/components/home/modules/FooterWidgets.jsx
@@ -3,112 +3,125 @@ import Link from "next/link";
 
 const FooterWidgets = () => (
   <div className="ps-footer__widgets">
-    <aside className="widget widget_footer widget_contact-us">
-      <h4 className="widget-title text-white">Contact us</h4>
-      <div className="widget_content">
-        <p>
-          <a href="mailto:info@shafn.com<" className="text-white">
-            info@shafn.com
-          </a>
-        </p>
-      </div>
-    </aside>
-    <aside className="widget widget_footer">
-      <h4 className="widget-title text-white">Quick links</h4>
-      <ul className="ps-list--link">
-        <li>
-        <Link href={"https://shafn.com/page/blank"} scroll={false}>
-            <a className="text-white">Policy</a>
-          </Link>
-        </li>
 
-        <li>
-        <Link href={"https://shafn.com/page/blank"} scroll={false}>
-            <a className="text-white">Term & Condition</a>
-          </Link>
-        </li>
-        <li>
-        <Link href={"https://shafn.com/page/blank"} scroll={false}>
-            <a className="text-white">Shipping</a>
-          </Link>
-        </li>
-        <li>
-        <Link href={"https://shafn.com/page/blank"} scroll={false}>
-            <a className="text-white">Return</a>
-          </Link>
-        </li>
-        <li>
-        <Link href={"https://shafn.com/page/faqs"} scroll={false}>
-            <a className="text-white">FAQs</a>
-          </Link>
-        </li>
-      </ul>
-    </aside>
-    <aside className="widget widget_footer">
-      <h4 className="widget-title text-white">Company</h4>
-      <ul className="ps-list--link">
-        <li>
-        <Link href={"https://shafn.com/page/about-us"} scroll={false}>
-            <a className="text-white">About Us</a>
-          </Link>
-        </li>
-        {/* <li>
-                    <Link href="/page/blank" scroll={false}>
+        <aside className="widget widget_footer">
+            <h4 className="widget-title text-white ">Contact us</h4>
+            <ul className="ps-list--link">
+               
+                <li>
+                    <Link legacyBehavior href="/return" scroll={false}>
+                        <a className="text-white">info@shafn.com</a>
+                    </Link>
+                </li>
+            </ul>
+        </aside>
+
+        <aside className="widget widget_footer">
+            <h4 className="widget-title text-white ">Quick links</h4>
+            <ul className="ps-list--link">
+                <li>
+                    <Link legacyBehavior href="/privacy-policy" scroll={false}>
+                        <a className="text-white">Policy</a>
+                    </Link>
+                </li>
+
+                <li>
+                    <Link legacyBehavior href="/terms-and-conditions" scroll={false}>
+                        <a className="text-white">Term & Condition</a>
+                    </Link>
+                </li>
+                <li>
+                    <Link legacyBehavior href="/shipping" scroll={false}>
+                        <a className="text-white">Shipping</a>
+                    </Link>
+                </li>
+                <li>
+                    <Link legacyBehavior href="/return" scroll={false}>
+                        <a className="text-white">Return</a>
+                    </Link>
+                </li>
+                <li>
+                    <Link legacyBehavior href="/page/faqs" scroll={false}>
+                        <a className="text-white">FAQs</a>
+                    </Link>
+                </li>
+            </ul>
+        </aside>
+
+        <aside className="widget widget_footer">
+            <h4 className="widget-title text-white">Company</h4>
+            <ul className="ps-list--link">
+                <li>
+                    <Link legacyBehavior href="/page/about-us" scroll={false}>
+                        <a className="text-white">About Us</a>
+                    </Link>
+                </li>
+                {/* <li>
+                    <Link legacyBehavior href="/page/blank" scroll={false}>
                         <a className="text-white">Affilate</a>
                     </Link>
                 </li>
                 <li>
-                    <Link href="/page/blank" scroll={false}>
+                    <Link legacyBehavior href="/page/blank" scroll={false}>
                         <a className="text-white">Career</a>
                     </Link>
                 </li> */}
-        <li>
-        <Link href={"https://shafn.com/page/contact-us"} scroll={false}>
-            <a className="text-white">Contact</a>
-          </Link>
-        </li>
-      </ul>
-    </aside>
-    <aside className="widget widget_footer">
-      <h4 className="widget-title text-white">Business</h4>
-      <ul className="ps-list--link">
-        {/* <li>
-                    <Link href="/page/about-us" scroll={false}>
+                <li>
+                    <Link legacyBehavior href="/page/contact-us" scroll={false}>
+                        <a className="text-white">Contact</a>
+                    </Link>
+                </li>
+            </ul>
+        </aside>
+
+        <aside className="widget widget_footer">
+            <h4 className="widget-title text-white">Business</h4>
+            <ul className="ps-list--link ">
+                {/* <li>
+                    <Link legacyBehavior href="/page/about-us" scroll={false}>
                         <a className="text-white">Our Press</a>
                     </Link>
                 </li> */}
-        <li>
-        <Link href={"https://shafn.com/account/checkout"} scroll={false}>
-            <a className="text-white">Checkout</a>
-          </Link>
-        </li>
-        <li>
-        <Link href={"https://shafn.com/shop"} scroll={false}>
-            <a className="text-white">Shop</a>
-          </Link>
-        </li>
-        <li>
-          <Link href={"https://www.seller.shafn.com/"} scroll={false}>
-            <a className="text-white">Become A Vendor</a>
-          </Link>
-        </li>
-        <li>
-        <Link href={"https://shafn.com/vendors"} scroll={false}>
-            <a className="text-white">Vendor Stores</a>
-          </Link>
-        </li>
-        <li>
-        <Link href={"https://shafn.com/account/order-tracking"} scroll={false}>
-            <a className="text-white">Track your order</a>
-          </Link>
-        </li>
-        <li>
-        <Link href={"https://shafn.com/account/register"} scroll={false}>
-            <a className="text-white">Sell on shafN</a>
-          </Link>
-        </li>
-      </ul>
-    </aside>
+                <li>
+                    <Link legacyBehavior href="/account/checkout" scroll={false}>
+                        <a className="text-white">Checkout</a>
+                    </Link>
+                </li>
+                {auth.isLoggedIn && (
+                    <li>
+                        <Link legacyBehavior href="/account/user-information" scroll={false}>
+                            <a className="text-white">My account</a>
+                        </Link>
+                    </li>
+                )}
+                <li>
+                    <Link legacyBehavior href="/shop" scroll={false}>
+                        <a className="text-white">Shop</a>
+                    </Link>
+                </li>
+                <li>
+                    <Link legacyBehavior href={"https://www.seller.shafn.com/"} scroll={false}>
+                        <a className="text-white added-bottom">Become A Vendor</a>
+                    </Link>
+                </li>
+                <li>
+                    <Link legacyBehavior href="/vendors" scroll={false}>
+                        <a className="text-white added-bottom">Vendor Stores</a>
+                    </Link>
+                </li>
+                <li>
+                    <Link legacyBehavior href="/account/order-tracking" scroll={false}>
+                        <a className="text-white added-bottom">Track your order</a>
+                    </Link>
+                </li>
+                <li>
+                    <Link legacyBehavior href="/account/register" scroll={false}>
+                        <a className="text-white added-bottom">Sell on shafN</a>
+                    </Link>
+                </li>
+            </ul>
+        </aside>
+
   </div>
 );
 
