5fa440c921d2b03cded2b6cb440991b665bdd6dc
Goodluck Edih <goodluckedih@yahoo.com>
2025-07-07 22:00:28 +0100

    updated home page

--- React-WordPress/app/components/v2/shared/BottomBar.tsx
+++ React-WordPress/app/components/v2/shared/BottomBar.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 
 export default function BottomBar() {
     return (
-        <div className="fixed bottom-[10px] z-[100] left-0 w-full">
+        <div className="fixed lg:hidden bottom-[10px] z-[100] left-0 w-full">
             <div className="bg-white text-[#242B36] flex p-3 items-center shadow-lg mx-auto left-0  w-10/12 rounded-[45px]">
                 <button className="flex-1 h-[36px] flex items-center justify-center"><i className="bi bi-list text-[28px]"></i></button>
                 <Link className="flex-1 h-[36px] flex items-center justify-center" href={"/"}><button><FavouriteIcon height={28} width={28}/></button></Link>
